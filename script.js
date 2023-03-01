617ed21d708106b67706c9886e4d6aec8622116c
divyashC <divyashchhetri@gmail.com>
2023-03-01 22:52:46 +0600

    Edit

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
-const track = document.getElementById("gallery");
+const track = document.getElementById("img-group");
 
+// function to handle the mouse wheel event and touch event that changes the image position
 const handleOnWheel = (e) => {
 	const deltaX = e.deltaX;
 	const deltaY = e.deltaY;
-	const maxDelta = window.innerWidth / 2;
+	const deltaMax = window.innerWidth / 2;
 
-	let percentage = (deltaX / maxDelta) * 100;
-	if (deltaY > 0) {
-		percentage += 5;
-	} else if (deltaY < 0) {
-		percentage -= 5;
-	}
+	let percentage =
+		(deltaX / deltaMax) * 100 + (deltaY > 0 ? 5 : deltaY < 0 ? -5 : 0);
 
-	const nextPercentageUnconstrained =
+	const nextUnconstrainedPercentage =
 		parseFloat(track.dataset.percentage || 0) + percentage;
 	const nextPercentage = Math.min(
-		Math.max(nextPercentageUnconstrained, -100),
+		Math.max(nextUnconstrainedPercentage, -100),
 		0
 	);
 
@@ -38,59 +35,48 @@ const handleOnWheel = (e) => {
 	}
 };
 
+// event listeners for mouse wheel and touch events
 window.addEventListener("wheel", handleOnWheel, { passive: true });
 
-window.addEventListener(
-	"touchstart",
-	(e) => {
-		track.dataset.mouseDownAt = e.touches[0].clientX;
-	},
-	{ passive: true }
-);
+window.addEventListener("touchstart", (e) => {
+	track.dataset.mouseDownAt = e.touches[0].clientX;
+});
 
-window.addEventListener(
-	"touchend",
-	(e) => {
-		track.dataset.mouseDownAt = "0";
-		track.dataset.prevPercentage = track.dataset.percentage;
-	},
-	{ passive: true }
-);
+window.addEventListener("touchend", (e) => {
+	track.dataset.mouseDownAt = "0";
+	track.dataset.prevPercentage = track.dataset.percentage;
+});
 
-window.addEventListener(
-	"touchmove",
-	(e) => {
-		if (track.dataset.mouseDownAt === "0") return;
+window.addEventListener("touchmove", (e) => {
+	if (track.dataset.mouseDownAt === "0") return;
 
-		const mouseDelta =
-			parseFloat(track.dataset.mouseDownAt) - e.touches[0].clientX;
-		const maxDelta = window.innerWidth / 2;
+	const deltaMouse =
+		parseFloat(track.dataset.mouseDownAt) - e.touches[0].clientX;
+	const deltaMax = window.innerWidth / 2;
 
-		const percentage = (mouseDelta / maxDelta) * -100;
-		const nextPercentageUnconstrained =
-			parseFloat(track.dataset.prevPercentage || 0) + percentage;
-		const nextPercentage = Math.max(
-			Math.min(nextPercentageUnconstrained, 0),
-			-100
-		);
+	const percentage = (deltaMouse / deltaMax) * -100;
+	const nextUnconstrainedPercentage =
+		parseFloat(track.dataset.prevPercentage || 0) + percentage;
+	const nextPercentage = Math.max(
+		Math.min(nextUnconstrainedPercentage, 0),
+		-100
+	);
 
-		track.dataset.percentage = nextPercentage;
+	track.dataset.percentage = nextPercentage;
 
-		track.animate(
+	track.animate(
+		{
+			transform: `translate(${nextPercentage}%, -50%)`,
+		},
+		{ duration: 1200, fill: "forwards" }
+	);
+
+	for (const image of track.getElementsByClassName("images")) {
+		image.animate(
 			{
-				transform: `translate(${nextPercentage}%, -50%)`,
+				objectPosition: `${100 + nextPercentage}% center`,
 			},
 			{ duration: 1200, fill: "forwards" }
 		);
-
-		for (const image of track.getElementsByClassName("images")) {
-			image.animate(
-				{
-					objectPosition: `${100 + nextPercentage}% center`,
-				},
-				{ duration: 1200, fill: "forwards" }
-			);
-		}
-	},
-	{ passive: true }
-);
+	}
+});
